fe508c9ac1330cf561808f9a97953c932a588104
kc980602 <160039700@stu.vtc.edu.hk>
2018-01-21 00:47:05 +0800

    My trips create & list trip plan

--- App/Triple/app/src/main/java/com/triple/triple/Mytrips/MytripsActivity.java
+++ App/Triple/app/src/main/java/com/triple/triple/Mytrips/MytripsActivity.java
@@ -9,11 +9,16 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.triple.triple.Home.HomeActivity;
 import com.triple.triple.R;
 import com.triple.triple.helper.BottomNavigationViewHelper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MytripsActivity extends AppCompatActivity {
 
@@ -31,6 +36,7 @@ public class MytripsActivity extends AppCompatActivity {
 
         setupBottomNavigationView();
         setupToolbar();
+        loadData();
     }
 
     /**
@@ -47,7 +53,7 @@ public class MytripsActivity extends AppCompatActivity {
     }
 
     /**
-     *Toolbar setup
+     * Toolbar setup
      */
     private void setupToolbar() {
         Log.d(TAG, "setupToolbar: setting up Toolbar");
@@ -71,6 +77,29 @@ public class MytripsActivity extends AppCompatActivity {
             startActivity(i_create);
             return true;
         }
-        return super.onOptionsItemSelected(item);
+        return true;
+    }
+
+    private void loadData() {
+        ListView data = (ListView) findViewById(R.id.data);
+        ArrayList<HashMap<String, Object>> listData = new ArrayList<HashMap<String, Object>>();
+        HashMap<String, Object> map = null;
+        int[] img = {R.drawable.a1, R.drawable.a2, R.drawable.a3, R.drawable.a4, R.drawable.a5, R.drawable.a5, R.drawable.a6};
+        for (int i=0; i<=10; i++) {
+            map = new HashMap<String, Object>();
+            map.put("tv_tripid", "111");
+            map.put("tv_tripname", "Fucking trip");
+            map.put("tv_owner", "Owner: asshole");
+            map.put("tv_tripdate", "1/7/1997 - 1/7/2047");
+            map.put("image1", img[(int) Math.ceil(Math.random() * 6)]);
+            map.put("image2", img[(int) Math.ceil(Math.random() * 6)]);
+            listData.add(map);
+        }
+
+        SimpleAdapter show = new SimpleAdapter(this, listData, R.layout.listviewitem_mytrips,
+                new String[]{"tv_tripname", "tv_owner", "tv_tripdate", "image1", "image2"},
+                new int[]{R.id.tv_tripname, R.id.tv_owner, R.id.tv_tripdate, R.id.image1, R.id.image2});
+        data.setAdapter(show);
+
     }
 }
