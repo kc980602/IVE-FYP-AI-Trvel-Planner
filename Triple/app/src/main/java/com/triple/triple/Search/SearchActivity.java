c898295c90c092921af8c1138d323d203e9bc920
kc980602 <160039700@stu.vtc.edu.hk>
2018-01-22 22:20:44 +0800

    My trips basic UI done

---
+++ Triple/app/src/main/java/com/triple/triple/Search/SearchActivity.java
@@ -0,0 +1,40 @@
+package com.triple.triple.Search;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+import com.triple.triple.R;
+import com.triple.triple.helper.BottomNavigationViewHelper;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private static final String TAG = "SearchActivity";
+    private static final int ACTIVITY_NUM = 1;
+
+    private Context mcontext = SearchActivity.this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        setupBottomNavigationView();
+    }
+
+    /**
+     * BottomNavigationView setup
+     */
+    private void setupBottomNavigationView() {
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(mcontext, bottomNavigationViewEx);
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
+    }
+}
