124070aa6aa8266ef2d888ced6a0aadb40ef45fc
kc980602 <160039700@stu.vtc.edu.hk>
2018-01-26 17:52:06 +0800

    add login

---
+++ Triple/app/src/main/java/com/triple/triple/Model/Adapter/TripDetailAdapter.java
@@ -0,0 +1,13 @@
+package com.triple.triple.Model.Adapter;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by KC on 1/26/2018.
+ */
+
+public class TripDetailAdapter extends BaseAdapter {
+
+
+
+}
