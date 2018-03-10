fd0301aca03fbf9defb55d17bfce1b35f1976133
Leung Lok Hay <lokhay00@gmail.com>
2018-03-11 00:05:42 +0800

    Updated GetAttractionDetail

--- Triple/app/src/main/java/com/triple/triple/Sync/GetAttractionDetail.java
+++ Triple/app/src/main/java/com/triple/triple/Sync/GetAttractionDetail.java
@@ -1,8 +1,10 @@
 package com.triple.triple.Sync;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.triple.triple.Interface.GitHubService;
+import com.triple.triple.Model.Attraction;
 
 import java.io.IOException;
 
@@ -12,6 +14,7 @@ import okhttp3.Response;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by Kevin on 2018/3/6.
@@ -31,24 +34,31 @@ public class GetAttractionDetail {
 //        try (Response response = client.newCall(request).execute()) {
 //            return response.body().string();
 //        }
-        Retrofit retrofit =new Retrofit.Builder()
+        Retrofit.Builder builder =new Retrofit.Builder()
                 .baseUrl(url)
-                .build();
+                .addConverterFactory(GsonConverterFactory.create());
+        Retrofit retrofit = builder.build();
         GitHubService client = retrofit.create(GitHubService.class);
-        Call<String> call = client.getInfo(1);
+        Call<Attraction> call = client.getRows();
 
-        call.enqueue(new Callback<String>() {
+        call.enqueue(new Callback<Attraction>() {
             @Override
-            public void onResponse(Call<String> call, retrofit2.Response<String> response) {
-                responseString = response.body();
+            public void onResponse(Call<Attraction> call, retrofit2.Response<Attraction> response) {
+                Attraction attraction = response.body();
+                if (response.body() != null){
+                    Log.i("onSuccess", response.body().toString());
+                    responseString = response.body().toString();
+                }else{
+                    Log.i("onEmptyResponse", "Returned empty response");//Toast.makeText(getContext(),"Nothing returned",Toast.LENGTH_LONG).show();
+                }
             }
 
             @Override
-            public void onFailure(Call<String> call, Throwable t) {
+            public void onFailure(Call<Attraction> call, Throwable throwable) {
 
             }
         });
 
-        return "responseString";
+        return responseString;
     }
 }
