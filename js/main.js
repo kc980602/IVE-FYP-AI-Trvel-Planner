e27117e3d7b96cc1ebcbd41bd7bfd4634c374e66
Leung Lok Hay <lokhay00@gmail.com>
2018-03-28 08:06:33 +0800

    First page complete

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  $('.modal').modal();
   $('select').material_select();
   $('#btnBW').prop('disabled', true)
   $('#step1').addClass('show').show()
@@ -27,11 +28,14 @@ $('#btnFW').click(function() {
   var gender      = $('input[name=gender]:checked').val();
   var ageGroup    = $('#ageGroup').val();
   var income      = $('#income').val();
-  var expenditure = $('#expenditure').val();
+  var nation      = $('#nation').val();
+  var education   = $('#education').val();
+
+
   var destination = $('#destination').val();
   if (step == 1 || step == undefined) {
     if(gender==undefined || ageGroup==null ||
-       income==null || expenditure==null )
+       income==null || nation==null || education==null )
       {
         alert("Please fill all required field");
               return false;
@@ -46,10 +50,18 @@ $('#btnFW').click(function() {
         $('#step2').show()
       }, 150)
       $(this).html('Submit <i class="material-icons right">send</i>')
+
+      $('.modal-content').children('p').html(
+        "<p>Gender\t:" + $('input[name=gender]:checked').val() + "</p>" +
+        "<p>Age Group\t:" + $('#ageGroup').val() + "</p>" +
+        "<p>Income\t:" + $('#income').val() + "</p>" +
+        "<p>Nationality\t:" + $('#nation').val() + "</p>" +
+        "<p>Education level\t:" + $('#education').val() + "</p>"
+      );
     }
   } else if (step == 2) {
     if (gender==undefined || ageGroup==null ||
-       income==null || expenditure==null || destination==null)
+       income==null || nation==null || education==null || destination==null)
        {
          alert("Please fill all required field");
                return false;
@@ -94,30 +106,30 @@ function valueToText(value){
   }
 }
 
-$('#btnGenerate').click(function() {
-  alert("Gender\t:" + $('input[name=gender]:checked').val() + "\n" +
-        "Age Group\t:" + $('#ageGroup').val() + "\n" +
-        "Income\t:" + $('#income').val() + "\n" +
-        "Occupation\t:" + $('#expenditure').val() + "\n" +
-        "\n" +
-        "Family Vacationer\t:" + valueToText($('#family_holiday_maker').val()) + "\n" +
-        "Foodie\t:" + valueToText($('#foodie').val()) + "\n" +
-        "Backpacker\t:" + valueToText($('#backpacker').val()) + "\n" +
-        "History Buff\t:" + valueToText($('#history_buff').val()) + "\n" +
-        "Nightlife Seeker\t:" + valueToText($('#nightlife_seeker').val()) + "\n" +
-        "Eco-tourist\t:" + valueToText($('#eco_tourist').val()) + "\n" +
-        "Trendsetter\t:" + valueToText($('#trendsetter').val()) + "\n" +
-        "Nature Lover\t:" + valueToText($('#nature_lover').val()) + "\n" +
-        "Thrifty Traveler\t:" + valueToText($('#thrifty_traveller').val()) + "\n" +
-        "Art and Architecture Lover\t:" + valueToText($('#art_and_architecture_lover').val()) + "\n" +
-        "Urban Explorer\t:" + valueToText($('#urban_explorer').val()) + "\n" +
-        "Thrill Seeker\t:" + valueToText($('#thrill_seeker').val()) + "\n" +
-        "Beach Goer\t:" + valueToText($('#beach_goer').val()) + "\n" +
-        "60+ Traveler\t:" + valueToText($('#sixtyPlus_traveller').val()) + "\n" +
-        "Like a Local\t:" + valueToText($('#like_a_local').val()) + "\n" +
-        "Luxury Traveller\t:" + valueToText($('#luxury_traveller').val()) + "\n" +
-        "Vegetarian\t:" + valueToText($('#vegetarian').val()) + "\n" +
-        "Shopping Fanatic\t:" + valueToText($('#shopping_fanatic').val()) + "\n" +
-        "Peace and Quiet See\t:" + valueToText($('#peace_and_quiet_seeker').val()) + "\n"
-  );
-});
+// $('#btnGenerate').click(function() {
+//   alert("Gender\t:" + $('input[name=gender]:checked').val() + "\n" +
+//         "Age Group\t:" + $('#ageGroup').val() + "\n" +
+//         "Income\t:" + $('#income').val() + "\n" +
+//         "Occupation\t:" + $('#expenditure').val() + "\n" +
+//         "\n" +
+//         "Family Vacationer\t:" + valueToText($('#family_holiday_maker').val()) + "\n" +
+//         "Foodie\t:" + valueToText($('#foodie').val()) + "\n" +
+//         "Backpacker\t:" + valueToText($('#backpacker').val()) + "\n" +
+//         "History Buff\t:" + valueToText($('#history_buff').val()) + "\n" +
+//         "Nightlife Seeker\t:" + valueToText($('#nightlife_seeker').val()) + "\n" +
+//         "Eco-tourist\t:" + valueToText($('#eco_tourist').val()) + "\n" +
+//         "Trendsetter\t:" + valueToText($('#trendsetter').val()) + "\n" +
+//         "Nature Lover\t:" + valueToText($('#nature_lover').val()) + "\n" +
+//         "Thrifty Traveler\t:" + valueToText($('#thrifty_traveller').val()) + "\n" +
+//         "Art and Architecture Lover\t:" + valueToText($('#art_and_architecture_lover').val()) + "\n" +
+//         "Urban Explorer\t:" + valueToText($('#urban_explorer').val()) + "\n" +
+//         "Thrill Seeker\t:" + valueToText($('#thrill_seeker').val()) + "\n" +
+//         "Beach Goer\t:" + valueToText($('#beach_goer').val()) + "\n" +
+//         "60+ Traveler\t:" + valueToText($('#sixtyPlus_traveller').val()) + "\n" +
+//         "Like a Local\t:" + valueToText($('#like_a_local').val()) + "\n" +
+//         "Luxury Traveller\t:" + valueToText($('#luxury_traveller').val()) + "\n" +
+//         "Vegetarian\t:" + valueToText($('#vegetarian').val()) + "\n" +
+//         "Shopping Fanatic\t:" + valueToText($('#shopping_fanatic').val()) + "\n" +
+//         "Peace and Quiet See\t:" + valueToText($('#peace_and_quiet_seeker').val()) + "\n"
+//   );
+// });
