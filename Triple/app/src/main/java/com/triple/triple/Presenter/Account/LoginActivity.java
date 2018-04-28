4e11f04ea28db1b0bba2446d5b75b535624aabf6
pokeguys <pokeguys@users.noreply.github.com>
2018-04-28 21:45:16 +0800

    Fixed Login UI
    1. Fixed UI
    2. Fixed error message

--- Triple/app/src/main/java/com/triple/triple/Presenter/Account/LoginActivity.java
+++ Triple/app/src/main/java/com/triple/triple/Presenter/Account/LoginActivity.java
@@ -6,46 +6,34 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.securepreferences.SecurePreferences;
+import com.triple.triple.Helper.ErrorUtils;
 import com.triple.triple.Interface.ApiInterface;
 import com.triple.triple.Model.AuthData;
-import com.triple.triple.Presenter.MainActivity;
+import com.triple.triple.Model.APIError;
 import com.triple.triple.Presenter.Registration.RegisterActivity;
 import com.triple.triple.R;
 import com.triple.triple.Sync.ApiClient;
-import com.triple.triple.Sync.Authentication;
 import com.wang.avi.AVLoadingIndicatorView;
-
-import java.lang.reflect.Type;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
-    private static final String TAG = "LoginActivity";
-
-    private Context mcontext = LoginActivity.this;
-
-    private Toolbar myToolbar;
-    private AVLoadingIndicatorView avi;
-    private AlertDialog dialog;
     private TextInputEditText et_username, et_password;
-    private String username, password;
     private ProgressDialog progressDialog;
     ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
 
@@ -54,14 +42,16 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        progressDialog = new ProgressDialog(mcontext);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
         progressDialog.setMessage(getString(R.string.dialog_progress_title));
 
-        setupToolbar();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         et_username = (TextInputEditText) findViewById(R.id.et_username);
         et_password = (TextInputEditText) findViewById(R.id.et_password);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
     }
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 1) {
@@ -69,12 +59,6 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         }
-    }
-
-    private void setupToolbar() {
-        myToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(myToolbar);
-        getSupportActionBar().setTitle("");
     }
 
     @Override
@@ -95,28 +79,27 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public boolean onButtonLoginClicked(View view) {
+        String username, password;
         int id = view.getId();
         if (id == R.id.bt_login) {
             username = et_username.getText().toString();
             password = et_password.getText().toString();
-//            new LoginActivity.RequestLogin().execute();
             requestLogin(username, password);
         }
         return true;
     }
 
     public void onClick(View view) {
-        int id = view.getId();
-        Intent indent = new Intent();
-        switch (id) {
-            case R.id.link_signup:
-                startActivityForResult(new Intent(mcontext, RegisterActivity.class), 1);
+        switch (view.getId()) {
+            case R.id.signup:
+                startActivityForResult(new Intent(this, RegisterActivity.class), 1);
                 return;
             case R.id.link_forget:
-                indent.setClass(mcontext, ForgetPasswordActivity.class);
+                startActivity(new Intent(this, ForgetPasswordActivity.class));
                 break;
+            default:
+                Log.e("action", "Invalid Action");
         }
-        startActivity(indent);
     }
 
     public void requestLogin(String username, String password) {
@@ -125,23 +108,18 @@ public class LoginActivity extends AppCompatActivity {
         call.enqueue(new Callback<AuthData>() {
             @Override
             public void onResponse(Call<AuthData> call, Response<AuthData> response) {
-                try {
-                    if (response.code() != 201) {
-                        AuthData auth = response.body();
-                        continueRequestLogin(auth);
-                    } else {
-                        stopRequestLogin();
-                    }
-                } catch (Exception e) {
-                    progressDialog.hide();
-                    Toast.makeText(mcontext, R.string.login_error_data, Toast.LENGTH_SHORT).show();
+                if (response.isSuccessful()){
+                    AuthData auth = response.body();
+                    continueRequestLogin(auth);
+                } else {
+                    APIError errorMessage = ErrorUtils.parseError(response);
+                    stopRequestLogin(errorMessage.getMessage());
                 }
-
             }
 
             @Override
             public void onFailure(Call<AuthData> call, Throwable t) {
-                stopRequestLogin();
+                stopRequestLogin(getString(R.string.login_error_process));
             }
         });
 
@@ -149,16 +127,15 @@ public class LoginActivity extends AppCompatActivity {
 
     public void continueRequestLogin(AuthData auth) {
         Gson gson = new Gson();
-        auth.toString();
         //save data using SharedPreferences
-        SharedPreferences data = new SecurePreferences(mcontext);
+        SharedPreferences data = new SecurePreferences(this);
         SharedPreferences.Editor editor = data.edit();
         editor.putString("token", auth.getToken());
         String json = gson.toJson(auth.getUser());
         editor.putString("userInfo", json);
-        editor.commit();
+        editor.apply();
         String message = getResources().getString(R.string.login_success) + ", " + auth.getUser().getUsername();
-        Toast.makeText(mcontext, message, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
         progressDialog.dismiss();
         Intent i = getBaseContext().getPackageManager()
                 .getLaunchIntentForPackage(getBaseContext().getPackageName());
@@ -168,9 +145,9 @@ public class LoginActivity extends AppCompatActivity {
         System.exit(0);
     }
 
-    public void stopRequestLogin() {
+    public void stopRequestLogin(String message) {
         progressDialog.hide();
-        Toast.makeText(mcontext, R.string.login_error_process, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
 }
